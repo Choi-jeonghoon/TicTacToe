426ce9378ddfd2a3de6cf8483c404b44fa3fdfd2
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2023-12-27 08:53:10 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        game-info
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
