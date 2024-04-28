c5b93b6418e9ec3bbcd049452b975616e6308f14
최정훈 <jeongssi94@gmail.com>
2024-04-28 21:28:36 +0900

    Ref: CSS 수정

--- src/App.js
+++ src/App.js
@@ -62,7 +62,9 @@ function App() {
     const desc = move ? "Go to move" + move : "Go to game start";
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{desc}</button>
+        <button className="move-button" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
       </li>
     );
   });
@@ -79,7 +81,7 @@ function App() {
       </div>
       <div className="game-info">
         <div className="status">{status}</div>
-        <ol>{moves}</ol>
+        <ol style={{ listStyle: "none" }}>{moves}</ol>
       </div>
     </div>
   );
